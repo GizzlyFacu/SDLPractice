741addd2a60b862f07abcc1013f145f4a3b520fb
GizzlyFacu <pamdulxe@gmail.com>
2025-03-19 18:12:07 -0300

    changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
+#define GLM_SWIZZLE_XYZW
+#define GLM_SWIZZLE_STQP 
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+
+
+int main() {
+    glm::vec3 VectorOne(3.0f);
+    glm::vec3 VectorTwo(0.5f);
+    glm::mat4 matrisde4(1.0f);
+
+    float pepe = glm::dot(glm::normalize(VectorOne), glm::normalize(VectorTwo));
+
+    std::cout << glm::to_string(matrisde4) << "\n";
+    std::cout <<"X de Vector es: " << VectorOne.x << "\n";
+
+    return 0;
+}
+/*
 #include <iostream>
 #include <SDL3/SDL.h>
 
@@ -32,3 +58,4 @@ int main() {
 	std::cout << "sas" << "\n";
 	return 0;
 }
+*/
